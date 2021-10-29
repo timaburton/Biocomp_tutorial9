30393ec66c9c8582c4d19cebaf3733cc914033ad
Marlee Shaffer <mshaffe2@nd.edu>
2021-10-29 10:16:04 -0400

    This commit creates the code to calculate the number of obersations for each species

--- Ex7Script.R
+++ Ex7Script.R
@@ -13,11 +13,14 @@ iris<-read.csv("iris.csv")
 #Print the last 2 rows and the last two columns to the R terminal
 
 #Get the number of observations for each species in the data set
+sum(iris[,5]=="virginica")
+sum(iris[,5]=="setosa")
+sum(iris[,5]=="versicolor")
 
 #Get rows with Sepal.Width > 3.5
-rowcount<- funtion(x,y,z){width<- (x[y>z,])return(width)}
-row(iris, iris$Sepal.Width, 3.5)
 
 #Write the data for species setosa to a comma-deliminated file named 'setosa.csv'
+setosa<-(iris[iris $"Species"=="setosa",])
+write.csv(setosa, 'setosa.csv')
 
 #Calculate the mean, minimum, and maximum of Petal.Length for observations from virginica
