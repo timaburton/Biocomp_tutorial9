f5e7c50d60208e199ab1d6b7a32db3cb847a2ebd
Marlee Shaffer <mshaffe2@nd.edu>
2021-10-29 10:13:06 -0400

    Adds code to create a csv for the species setosa

--- Ex7Script.R
+++ Ex7Script.R
@@ -1,3 +1,23 @@
 ###Exercise 7 Script 
 ###Marlee Shaffer
 
+setwd("~/Desktop/Biocomp_tutorial9")
+iris<-read.csv("iris.csv")
+
+##Part 1: Replicate the functionality fo the head function in bash 
+
+
+
+#Part 2: Use iris.csv to: 
+
+#Print the last 2 rows and the last two columns to the R terminal
+
+#Get the number of observations for each species in the data set
+
+#Get rows with Sepal.Width > 3.5
+rowcount<- funtion(x,y,z){width<- (x[y>z,])return(width)}
+row(iris, iris$Sepal.Width, 3.5)
+
+#Write the data for species setosa to a comma-deliminated file named 'setosa.csv'
+
+#Calculate the mean, minimum, and maximum of Petal.Length for observations from virginica
