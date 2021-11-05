17ab26c8c074eda8df6df0d9306f4bd5b1c7c613
selidris <92063773+selidris@users.noreply.github.com>
2021-11-04 20:40:54 -0400

    Add files via upload

--- Exercise7.R
+++ Exercise7.R
@@ -20,20 +20,24 @@ iris[149:150,4:5]
 
 #get the number of observations for each species included in the data set
 sumspecies<-iris[order(iris$Species, decreasing=TRUE),]
+
 sum(sumspecies$Species=="virginica")
 sum(sumspecies$Species=="versicolor")
 sum(sumspecies$Species=="setosa")
 
 #get rows with Sepal.Width > 3.5
 sw<-(iris[iris$Sepal.Width>3.5,])
+
 sw
 
 #write the data for the species setosa to a comma-delimited file names ‘setosa.csv’
 setosa<-subset(iris, Species=="setosa")
+
 write.csv(setosa, file="setosa.csv", row.names=FALSE)
 
 #calculate the mean, minimum, and maximum of Petal.Length for observations from virginica
 v=iris[iris$Species=="virginica",]
+
 mean(v$Petal.Length)
 max(v$Petal.Length)
 min(v$Petal.Length) 
